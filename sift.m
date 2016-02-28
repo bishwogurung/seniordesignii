810fd3b701719bd8fff1f735136a36564775181c
Bishwo Gurung <bishwo.gurung3@gmail.com>
2016-02-27 23:16:27 -0500

    initiating project

---
+++ sift.m
@@ -0,0 +1,29 @@
+% %%# Read an image
+% I = imread('bread.jpg');
+% %# Create the gaussian filter with hsize = [5 5] and sigma = 2
+% G = fspecial('gaussian',[40 40],40);
+% %# Filter it
+% Ig = imfilter(I,G,'same');
+% %# Display
+% imshow(Ig)
+
+I = imread('bread.jpg');
+I=rgb2gray(I);
+I = imresize(I, 0.1);
+% I = imgaussfilt(I, 2);
+% imshow(I)
+% S = imshow(I)
+imshow(I)
+i=1;
+% imshow(I)
+% while i~=5
+%     B=imresize(I, 0.5);
+% %     imshowpair(I, B, 'montage')
+% imshow(B)
+%     i=i+1;
+% end
+while i~=5
+    blur = imgaussfilt(I, 2);
+    imshow(I)
+    i=i+1;
+end
