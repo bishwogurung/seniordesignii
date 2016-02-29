00d34a9595b91b78c87ed8a6920c0f02f48ba3aa
Bishwo Gurung <bishwo.gurung3@gmail.com>
2016-02-29 07:13:28 -0500

    added 5 octaves and DoG

--- sift.m
+++ sift.m
@@ -1,21 +1,21 @@
-% %%# Read an image
-% I = imread('bread.jpg');
-% %# Create the gaussian filter with hsize = [5 5] and sigma = 2
-% G = fspecial('gaussian',[40 40],40);
-% %# Filter it
-% Ig = imfilter(I,G,'same');
-% %# Display
-% imshow(Ig)
+% % %%# Read an image
+% % I = imread('bread.jpg');
+% % %# Create the gaussian filter with hsize = [5 5] and sigma = 2
+% % G = fspecial('gaussian',[40 40],40);
+% % %# Filter it
+% % Ig = imfilter(I,G,'same');
+% % %# Display
+% % imshow(Ig)
+% 
+% 
+% 
+% % imshow('cameraman.tif')
+% 
+% 
+% 
+% 
 
-
-
-% imshow('cameraman.tif')
-
-
-
-
-
-I = imread('cameraman.tif');
+I = imread('gingerbread.jpg');
 if size(I, 3)==3
     I=rgb2gray(I);
 end
@@ -33,23 +33,54 @@ end
 %     i=i+1;
 % % end
 sigma = sqrt(2);
-blur_1 = imgaussfilt(I, sigma);
+% 
+% blur_1 = imgaussfilt(I, sigma);
+% 
+% blur_2 = imgaussfilt(blur_1, sigma);
+% 
+% blur_3 = imgaussfilt(blur_2, sigma);
+% 
+% blur_4 = imgaussfilt(blur_3, sigma);
+% 
+% subplot(3,2,1); imshow(I)
+% subplot(3,2,2); imshow(blur_1)
+% subplot(3,2,3); imshow(blur_2)
+% subplot(3,2,4); imshow(blur_3)
+% subplot(3,2,5); imshow(blur_4)
 
-blur_2 = imgaussfilt(blur_1, sigma);
+max_octave = 5;
+for i=1:max_octave
+    I= imresize(I, 1/2*i);
+    blur_1 = imgaussfilt(I, sigma);
 
-blur_3 = imgaussfilt(blur_2, sigma);
+    blur_2 = imgaussfilt(blur_1, sigma);
 
-blur_4 = imgaussfilt(blur_3, sigma);
+    blur_3 = imgaussfilt(blur_2, sigma);
 
-subplot(3,2,1); imshow(I)
-subplot(3,2,2); imshow(blur_1)
-subplot(3,2,3); imshow(blur_2)
-subplot(3,2,4); imshow(blur_3)
-subplot(3,2,5); imshow(blur_4)
+    blur_4 = imgaussfilt(blur_3, sigma);
 
-% while i~=6
-%     blur = imgaussfilt(I, sigma);
-%     imshow(blur)
-%     3
-%     i=i+1;
-% end
+    subplot(3,2,1); imshow(I)
+    subplot(3,2,2); imshow(blur_1)
+    subplot(3,2,3); imshow(blur_2)
+    subplot(3,2,4); imshow(blur_3)
+    subplot(3,2,5); imshow(blur_4)
+    
+    
+        figure;
+   
+    
+    DoG_1 = I-blur_1;
+    subplot(2,2,1); imshow(DoG_1)
+    DoG_2 = blur_1-blur_2;
+    subplot(2,2,2); imshow(DoG_1)
+    DoG_3 = blur_2-blur_3;
+    subplot(2,2,3); imshow(DoG_1)
+    DoG_4 = blur_3-blur_4;
+    subplot(2,2,4); imshow(DoG_1)
+        
+        figure;
+
+
+
+    
+end
