d7113066f4ec744bff1adccd347174ead1edbdd0
Bishwo Gurung <bishwo.gurung3@gmail.com>
2016-04-10 23:57:21 -0400

    testing hogsvm

--- hog_svm.m
+++ hog_svm.m
@@ -2,43 +2,30 @@ rootFolder = fullfile('/Users/Bishwo/Downloads/101_ObjectCategories');
 
 imgSets = [ imageSet(fullfile(rootFolder, 'airplanes')), ...
             imageSet(fullfile(rootFolder, 'ferry')), ...
-            imageSet(fullfile(rootFolder, 'laptop')) ];      
+            imageSet(fullfile(rootFolder, 'starfish')) ];      
 
 minSetCount = min([imgSets.Count]);
 
 imgSets = partition(imgSets, minSetCount, 'randomize');
 
-% for image = 1:numel(imgSets)
-% %     image = imresize(image, [20 20]);
-% end
-%     %numel - total number of array elements
-
 [trainingSets, testSets] = partition(imgSets, 0.3, 'randomize');
 
 img = read(trainingSets(1), 2);
-train_img_size = size(img);
+train_img_size = [size(img, 1), size(img, 2)];
+
+for pic = 1:numel(testSets)
+    numImages = testSets(pic).Count;
+    
+    for i = 1:numImages
+        img = read(testSets(pic), i);
+        img = imresize(img, train_img_size);
+        lvl = graythresh(img);
+        img = im2bw(img, lvl);
+    end
+end
 
 % Extract HOG features and HOG visualization
-[hog_2x2, vis2x2] = extractHOGFeatures(img,'CellSize',[2 2]);
 [hog_4x4, vis4x4] = extractHOGFeatures(img,'CellSize',[4 4]);
-[hog_8x8, vis8x8] = extractHOGFeatures(img,'CellSize',[8 8]);
-
-% Show the original image
-% figure;
-% subplot(2,3,1:3); imshow(img);
-% 
-% % Visualize the HOG features
-% subplot(2,3,4);
-% plot(vis2x2);
-% title({'CellSize = [2 2]'; ['Feature length = ' num2str(length(hog_2x2))]});
-% 
-% subplot(2,3,5);
-% plot(vis4x4);
-% title({'CellSize = [4 4]'; ['Feature length = ' num2str(length(hog_4x4))]});
-% 
-% subplot(2,3,6);
-% plot(vis8x8);
-% title({'CellSize = [8 8]'; ['Feature length = ' num2str(length(hog_8x8))]});
 
 cellSize = [4 4];
 hogFeatureSize = length(hog_4x4);
@@ -46,46 +33,33 @@ hogFeatureSize = length(hog_4x4);
 trainingFeatures = [];
 trainingLabels   = [];
 
-for image = 1:numel(trainingSets)
+for pic = 1:numel(trainingSets)
 
-    numImages = trainingSets(image).Count;
+    numImages = trainingSets(pic).Count;
     features  = zeros(numImages, hogFeatureSize, 'single');
   
-    
     for i = 1:numImages
-%         img = rgb2gray(read(trainingSets(image), i));
-%         img = imbinarize(img);
-        
-        img = read(trainingSets(image), i);
+        img = read(trainingSets(pic), i);
         img = imresize(img, train_img_size);
         lvl = graythresh(img);
         img = im2bw(img, lvl);
-%         img = im2single(img);
 
-%         img =imresize(img, [25 25]);
-%         first = extractHOGFeatures(img, 'CellSize', cellSize);
-%         display('first')
-%         size(first)
-%         display('hog_4x4')
-%         size(hog_4x4)
         features(i, :) = extractHOGFeatures(img, 'CellSize', cellSize);
-        size(features)
     end
     
-    labels = repmat(trainingSets(image).Description, numImages, 1);
+    labels = repmat({trainingSets(pic).Description}, numImages, 1);
     
     trainingFeatures = [trainingFeatures; features];
     trainingLabels = [trainingLabels; labels];
 end
 
 classifier = fitcecoc(trainingFeatures, trainingLabels);
-   
-[testFeatures, testLabels] = helperExtractHOGFeaturesFromImageSet(testSets, hogFeatureSize, cellSize);
 
+[testFeatures, testLabels] = helperExtractHOGFeaturesFromImageSet(testSets, hogFeatureSize, cellSize);
 % Make class predictions using the test features.
 predictedLabels = predict(classifier, testFeatures);
 
 % Tabulate the results using a confusion matrix.
 confMat = confusionmat(testLabels, predictedLabels);
 
-helperDisplayConfusionMatrix(confMat)
+% helperDisplayConfusionMatrix(confMat)
