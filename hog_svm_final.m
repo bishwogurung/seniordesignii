0e182959ed7e7aa5543e6873b7b0aed3e8ffeebc
shifalik <shifalikumar17@gmail.com>
2016-04-18 12:05:30 -0400

    updated txt to speech for windows

--- hog_svm_final.m
+++ hog_svm_final.m
@@ -1,4 +1,24 @@
+<<<<<<< HEAD
+%%
+%We are using Caltech 101 image data set for our reference pictures.
+
+url = 'http://www.vision.caltech.edu/Image_Datasets/Caltech101/101_ObjectCategories.tar.gz';
+%using the link from the internet 
+
+outputFolder = fullfile(tempdir, 'caltech101'); 
+%downloading the caltech files 
+
+if ~exist(outputFolder, 'dir') 
+    disp('Downloading 126MB Caltech101 data set...');
+    untar(url, outputFolder);
+end
+%'if' loop incase we re-run our code, this should download the images only once
+
+
+rootFolder = fullfile(outputFolder, '101_ObjectCategories');
+=======
 rootFolder = fullfile('/Users/Bishwo/Downloads/101_ObjectCategories');
+>>>>>>> faa04ab4d957ced937d41bb787168105acff0aee
 
 imgSets = [ imageSet(fullfile(rootFolder, 'airplanes')), ...
             imageSet(fullfile(rootFolder, 'cup')), ...
@@ -45,7 +65,11 @@ hogFeatureSize = length(hog_4x4);
 
 trainingFeatures = [];
 trainingLabels   = [];
+<<<<<<< HEAD
+
+=======
 tic
+>>>>>>> faa04ab4d957ced937d41bb787168105acff0aee
 for pic = 1:numel(trainingSets)
 
     numImages = trainingSets(pic).Count;
@@ -65,11 +89,16 @@ for pic = 1:numel(trainingSets)
     trainingFeatures = [trainingFeatures; features];
     trainingLabels = [trainingLabels; labels];
 end
+<<<<<<< HEAD
+
+classifier = fitcecoc(trainingFeatures, trainingLabels);
+=======
 toc
 
 tic
 classifier = fitcecoc(trainingFeatures, trainingLabels);
 toc
+>>>>>>> faa04ab4d957ced937d41bb787168105acff0aee
 % categoryClassifier = trainImageCategoryClassifier(trainingSets, trainingFeatures);
 % confMatrix = evaluate(categoryClassifier, testSets)
 % testpic = imread('http://d111vui60acwyt.cloudfront.net/product_photos/3169638/Profile(1)_original.jpg');%cup
@@ -78,7 +107,11 @@ toc
 % testpic = imread('http://cdn.history.com/sites/2/2013/12/egyptian-pyramids-hero-H.jpeg'); %pyramid
 % testpic = imread('test_image5.jpeg'); %pyramids
 % testpic = imread('img2.jpg');
+<<<<<<< HEAD
+testpic = imread('test_image1.jpg');
+=======
 testpic = imread('test_image4.jpg');
+>>>>>>> faa04ab4d957ced937d41bb787168105acff0aee
 
 testpic = imresize(testpic, train_img_size);
 lvl = graythresh(testpic);
@@ -92,6 +125,23 @@ confMat = confusionmat(testlabel, predictedlabel);
 
 [c, order] = confusionmat(testlabel, predictedlabel);
 order{2}
+<<<<<<< HEAD
+% answer = ['say ' order{2}];
+% system(answer);
+% clearvars
+
+%%%%%%%%%
+%windows text to speech addition 
+if isempty(predictedlabel)
+	return;
+end; 
+caUserInput = char(predictedlabel); % Convert from cell to string.
+NET.addAssembly('System.Speech');
+obj = System.Speech.Synthesis.SpeechSynthesizer;
+obj.Volume = 100;
+Speak(obj, caUserInput);
+=======
 answer = ['say ' order{2}];
 system(answer);
 % clearvars
+>>>>>>> faa04ab4d957ced937d41bb787168105acff0aee
